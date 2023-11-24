51915a267e805b54cef0ff9ab3f145622c30b935
JessicaRodriguez <tami.jessica.rodriguez@gmail.com>
2023-11-24 15:33:41 +0100

    added welcome time, month and day

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,6 +2,7 @@ window.addEventListener("DOMContentLoaded", main);
 
 function main() {
   startTodos();
+  initWelcome();
   // console.log('Hi students!');
   // Init today view
   // Init todos list
---
+++ public/javascript/welcome.js
@@ -0,0 +1,56 @@
+function initWelcome() {
+  //   showCurrentDay();
+  updateDateTime();
+}
+
+function updateDateTime() {
+  const now = new Date();
+  const days = [
+    "Söndag",
+    "Måndag",
+    "Tisdag",
+    "Onsdag",
+    "Torsdag",
+    "Fredag",
+    "Lördag",
+  ];
+  const months = [
+    "Januari",
+    "Februari",
+    "Mars",
+    "April",
+    "Maj",
+    "Juni",
+    "Juli",
+    "Augusti",
+    "September",
+    "Oktober",
+    "November",
+    "December",
+  ];
+
+  // Format the day, date, and month
+  const dayOfWeek = days[now.getDay()];
+  const date = now.getDate();
+  const month = months[now.getMonth()];
+  const year = now.getFullYear();
+
+  // Display the full date
+  const todaysDateElement = document.getElementById("todaysDate");
+  todaysDateElement.textContent = `${dayOfWeek} ${date} ${month} ${year}`;
+
+  // Format the time
+  const timeElement = document.getElementById("time");
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+
+  // Display the current time
+  timeElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+// Call `updateDateTime` on page load and then every second
+document.addEventListener("DOMContentLoaded", function () {
+  updateDateTime();
+  setInterval(updateDateTime, 1000);
+});
