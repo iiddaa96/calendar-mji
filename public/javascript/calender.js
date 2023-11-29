c839cc518b3ede5729591d321941de435e6f0545
JessicaRodriguez <tami.jessica.rodriguez@gmail.com>
2023-11-29 15:41:26 +0100

    new code for calendar view

--- public/javascript/calender.js
+++ public/javascript/calender.js
@@ -1,140 +1,223 @@
-function initCalender() {
-  generateCalenderDays();
-  
-  //test
-  // calenderInfo();
-  // changeBackgroundByMonth();
+// window.addEventListener("DOMContentLoaded", initCalendar);
+
+function initCalendar() {
+  renderCalenderDays();
+  calenderInfo();
+  changeBackgroundByMonth();
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const displayCurrentMonth = document.getElementById("displayCurrentMonth");
-  const calendar = document.querySelector(".calendar");
-  let currentDate = new Date();
+/** Creates an object with the current date, year, month and day. */
+let calendar = {
+  date: new Date(),
+  year: null,
+  month: null,
+  day: null,
+};
 
-  function updateMonthDisplay() {
-    displayCurrentMonth.textContent = currentDate.toLocaleDateString("sv-SE", {
-      month: "long",
-      year: "numeric",
-    });
-  }
+calendar.year = calendar.date.getFullYear();
+calendar.month = calendar.date.getMonth();
+calendar.day = calendar.date.getDate();
 
-  async function getHolidayAPI() {
-    try {
-      const url = `https://sholiday.faboul.se/dagar/v2.1/${currentDate.getFullYear()}/${
-        currentDate.getMonth() + 1
-      }`;
-      const response = await fetch(url);
+/** Array with the months of the year.*/
+const months = [
+  "Januari",
+  "Februari",
+  "Mars",
+  "April",
+  "Maj",
+  "Juni",
+  "Juli",
+  "Augusti",
+  "September",
+  "Oktober",
+  "November",
+  "December",
+];
 
-      const days = result.dagar;
+/**Array with the weekdays */
+// const weekdays = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag"];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-      const fetchedHolidays = [];
-      for (let i = 0; i < days.length; i++) {
-        if (days[i].helgdag) {
-          fetchedHolidays.push(days[i]);
-        }
-      }
-      return fetchedHolidays;
-    } catch (error) {
-      console.error("Error fetching holiday data:", error);
-      throw error; // Rethrow the error to propagate it further
+async function getHolidayAPI() {
+  const url = `https://sholiday.faboul.se/dagar/v2.1/${calendar.year}/${
+    calendar.month + 1
+  }`;
+  const response = await fetch(url);
+  const result = await response.json();
+
+  const days = result.dagar;
+
+  const fetchedHolidays = [];
+  for (let i = 0; i < days.length; i++) {
+    if (days[i].helgdag) {
+      fetchedHolidays.push(days[i]);
     }
   }
+  return fetchedHolidays;
+}
 
-  async function generateCalenderDays() {
-    const firstDayOfMonth = new Date(
-      currentDate.getFullYear(),
-      currentDate.getMonth(),
-      1
-    );
-    const lastDayOfMonth = new Date(
-      currentDate.getFullYear(),
-      currentDate.getMonth() + 1,
-      0
-    );
+async function renderCalenderDays() {
+  let calenderUL = document.querySelector(".calendar");
 
-    const now = new Date();
+  let firstWeekDayOfMonth = new Date(
+    calendar.year,
+    calendar.month,
+    1,
+    -1
+  ).getDay(); // Getting first weekday of mounth
+  let lastDateOfMonth = new Date(
+    calendar.year,
+    calendar.month + 1,
+    0
+  ).getDate(); // Getting last date of month
+  let lastDayOfMonth = new Date(
+    calendar.year,
+    calendar.month,
+    lastDateOfMonth - 1
+  ).getDay(); // Getting last date of month
+  let lastDateOfPrevMonth = new Date(
+    calendar.year,
+    calendar.month,
+    0
+  ).getDate(); // Getting last date of prev month
 
-    getHolidayAPI().then((holidays) => {
-      let liTag = "";
+  const now = new Date();
 
-      const lastDateOfPrevMonth = new Date(
-        currentDate.getFullYear(),
-        currentDate.getMonth(),
-        0
-      ).getDate();
-      // loop for padding days of previous month
-      for (let i = firstDayOfMonth.getDay(); i > 0; i--) {
-        liTag += `<li class="padding-days">${lastDateOfPrevMonth - i + 1}</li>`;
-      }
-
-      // Iterates the current month and adds the days to the calendar
-      for (let i = 1; i <= lastDayOfMonth.getDate(); i++) {
-        const currentDate =
-          calendar.year +
-          "-" +
-          ("" + (calendar.month + 1)).padStart(2, "0") +
-          "-" +
-          ("" + i).padStart(2, "0");
-
-        let isToday =
-          i === calendar.day &&
-          calendar.month === now.getMonth() &&
-          calendar.year === now.getFullYear()
-            ? "activeDay"
-            : "";
-        let holidayString = "";
-
-        const xx = holidays.filter((h) => {
-          return h.datum === currentDate;
-        });
-
-        if (xx[0]) {
-          holidayString = xx[0].helgdag;
-        }
-
-        liTag += `<li class="${isToday}">${i}<p>${holidayString}</p></li>`;
-      }
-      // Creating li of next month first days
-      for (let i = lastDayOfMonth.getDate(); i < 6; i++) {
-        liTag += `<li class="padding-days">${
-          i - lastDayOfMonth.getDate() + 1
-        }</li>`;
-      }
-
-      calendar.innerHTML = liTag;
-    });
-
-    // Clear the calendar
-    calendar.innerHTML = "";
-
-    // Add empty cells at the start of the calendar
-    for (let i = 0; i < firstDayOfMonth.getDay(); i++) {
-      calendar.insertAdjacentHTML("beforeend", '<li class="empty-day"></li>');
+  getHolidayAPI().then((holidays) => {
+    let liTag = "";
+    // loop for padding days of previous month
+    for (let i = firstWeekDayOfMonth; i > 0; i--) {
+      liTag += `<li class="padding-days">${lastDateOfPrevMonth - i + 1}</li>`;
     }
 
-    // Fill in the days of the month
-    for (let day = 1; day <= lastDayOfMonth.getDate(); day++) {
-      const dayItem = document.createElement("li");
-      dayItem.textContent = day;
-      calendar.appendChild(dayItem);
+    // Iterates the current month and adds the days to the calendar
+    for (let i = 1; i <= lastDateOfMonth; i++) {
+      const currentDate =
+        calendar.year +
+        "-" +
+        ("" + (calendar.month + 1)).padStart(2, "0") +
+        "-" +
+        ("" + i).padStart(2, "0");
+
+      let isToday =
+        i === calendar.day &&
+        calendar.month === now.getMonth() &&
+        calendar.year === now.getFullYear()
+          ? "activeDay"
+          : "";
+      let holidayString = "";
+
+      const xx = holidays.filter((h) => {
+        return h.datum === currentDate;
+      });
+
+      if (xx[0]) {
+        holidayString = xx[0].helgdag;
+      }
+
+      liTag += `<li class="${isToday}">${i}<p>${holidayString}</p></li>`;
     }
-  }
+    // Creating li of next month first days
+    for (let i = lastDayOfMonth; i < 6; i++) {
+      liTag += `<li class="padding-days">${i - lastDayOfMonth + 1}</li>`;
+    }
 
-  function navigateMonth(offset) {
-    currentDate.setMonth(currentDate.getMonth() + offset);
-    updateMonthDisplay();
-    generateCalenderDays();
-  }
+    calenderUL.innerHTML = liTag;
+  });
+}
 
-  // Event listeners for navigation arrows
+/**
+ * Gets the current year, month and day and displays the current month in the calendar.
+ */
+
+function calenderInfo() {
+  changeMonths();
+
+  drawCurrentMonth();
+}
+
+/**
+ * Adds event listeners to the buttons that changes the month.
+ */
+function changeMonths() {
   document
     .getElementById("monthBackArrow")
-    .addEventListener("click", () => navigateMonth(-1));
+    .addEventListener("click", monthBack);
   document
     .getElementById("monthForwardArrow")
-    .addEventListener("click", () => navigateMonth(1));
+    .addEventListener("click", monthForward);
+}
 
-  // Initial setup
-  updateMonthDisplay();
-  generateCalenderDays();
-});
+/**
+ * If the month is december, the month will be set to january and the year will be increased by one.
+ */
+function monthForward() {
+  if (calendar.month === 11) {
+    calendar.month = 0;
+    calendar.year++;
+  } else {
+    calendar.month++;
+  }
+  renderCalenderDays();
+  drawCurrentMonth();
+  changeBackgroundByMonth();
+}
+
+/**
+ * If the month is january, the month will be set to december and the year will be decreased by one.
+ */
+function monthBack() {
+  if (calendar.month === 0) {
+    calendar.month = 11;
+    calendar.year--;
+  } else {
+    calendar.month--;
+  }
+  renderCalenderDays();
+  drawCurrentMonth();
+  changeBackgroundByMonth();
+}
+/**
+ * Displays the current month and year in the calendar.
+ */
+function drawCurrentMonth() {
+  document.getElementById("displayCurrentMonth").innerText =
+    months[calendar.month] + " " + +calendar.year;
+}
+
+// Funktion för att ändra bakgrundsbilden baserat på månaden
+function changeBackgroundByMonth() {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth();
+
+  // Array med filnamn för bakgrundsbilder
+  const backgroundImages = [
+    "january.jpg",
+    "february.jpg",
+    "march.jpg",
+    "april.jpg",
+    "may.jpg",
+    "june.jpg",
+    "july.jpg",
+    "august.jpg",
+    "september.jpg",
+    "october.jpg",
+    "winter3.jpg",
+    "november.jpg",
+  ];
+
+  //Använd månaden som index för att hämta rätt bildfilnamn
+  const backgroundUrl = `url("/images/${backgroundImages[currentMonth]}")`;
+  // const backgroundUrl = `url("/path/to/images/${backgroundImages[currentMonth]}")`;
+
+  // Tilldela bakgrundsbilden till kroppselementet
+  document.body.style.backgroundImage = backgroundUrl;
+}
